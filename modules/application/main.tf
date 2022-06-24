49740109dc0cb0e783429dabb16e6d492ab2a5cb
samuelbartels20 <bartelssamuel20@gmail.com>
2022-06-24 12:44:43 +0000

    added ami resource block on security module

---
+++ modules/application/main.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Canonical
+  owners = ["099720109477"]
+}
