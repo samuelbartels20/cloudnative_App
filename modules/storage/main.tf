c85cdf491fecf41d6a1fcc5a058ce3978aceb993
samuelbartels20 <bartelssamuel20@gmail.com>
2022-06-24 09:56:28 +0000

    added instance resource for storage module

---
+++ modules/storage/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "cloudnativeApp_mongo" {
+  ami                    = "ami-02868af3c3df4b3aa"
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.sg_id]
+
+  user_data = filebase64("${path.module}/install.sh")
+
+  tags = {
+    Name  = "Mongo"
+  }
+}
